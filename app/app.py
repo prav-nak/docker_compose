132d95cb8cdb6d2ae96bce1c46826e83b01d5ddc
thaveen <thaveen15@ggmail.com>
2020-03-26 23:36:22 -0500

    added pandas dataframe table display to flask.

--- app/app.py
+++ app/app.py
@@ -2,23 +2,59 @@ from typing import List, Dict
 from flask import Flask
 import mysql.connector
 import json
+from flask import request, render_template, session, redirect, url_for
+import pandas as pd
+
 
 app = Flask(__name__)
 
-def people_data() -> List[Dict]:
+df = pd.DataFrame({'A': [0, 1, 2, 3, 4],
+                   'B': [5, 6, 7, 8, 9],
+                   'C': ['a', 'b', 'c--', 'd', 'e']})
+
+def people_data():
     config = { 'user': 'root', 'password': 'root', 'host': 'db', 'port': '3306', 'database': 'census' }
     connection = mysql.connector.connect(**config)
     cursor = connection.cursor()
     cursor.execute('SELECT * FROM people_data')
-    results = [{name: [gender, age]} for (name, gender, age) in cursor]
+    
+    dfObj = pd.DataFrame(columns=["Name", "Gender", "Age"])
+    for (name, gender, age) in cursor:
+        dfObj = dfObj.append({'Name' : name , 'Gender' : gender, 'Age' : age} , ignore_index=True)
+
     cursor.close()
     connection.close()
 
-    return results
+    return dfObj
 
-@app.route('/')
-def index() -> str:
-    return json.dumps({'people_data': people_data()})
+def inject_data(name, gender, age):
+    config = { 'user': 'root', 'password': 'root', 'host': 'db', 'port': '3306', 'database': 'census' }
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO people_data VALUES ("{name}", "{gender}", {age})'.format(name=name, gender=gender, age=int(age)))
+
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+    return 
+
+@app.route('/', methods=["GET"])
+def html_table():
+
+    dfObj = people_data()
+
+    return render_template('simple.html',  tables=[dfObj.to_html(classes="data")], titles=dfObj.columns.values)
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    name, gender, age = processed_text.split(",")
+
+    inject_data(name, gender, age)
+
+    return redirect(url_for('html_table'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
